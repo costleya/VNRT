79016709ca5a14d0af176e27ae4bd8841eac67b3
weapon009 <costley.aaron@gmail.com>
2014-01-25 14:59:22 -0600

    Basic Structure

--- Visual Novel Editor/App1/MainPage.xaml.cs
+++ Visual Novel Editor/App1/MainPage.xaml.cs
@@ -15,7 +15,7 @@ using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
-namespace App1
+namespace Vnrt.Editor
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
@@ -25,7 +25,6 @@ namespace App1
         public MainPage()
         {
             this.InitializeComponent();
-            int x = 0;
         }
     }
 }
