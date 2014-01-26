ca5f28bb057c8335671de4c24577417a17e890f6
Omar007 <omarpakker@gmail.com>
2014-01-26 18:31:57 +0100

    Some more reference fixes (stuff that was still named App1)

--- Visual Novel Editor/Visual Novel Editor/Properties/AssemblyInfo.cs
+++ Visual Novel Editor/Visual Novel Editor/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("App1")]
+[assembly: AssemblyTitle("Visual Novel Editor")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("App1")]
+[assembly: AssemblyProduct("Visual Novel Editor")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
