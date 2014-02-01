0bc39a54bd6e08aa4bb32afdee071bbb4acaff9f
Omar007 <omarpakker@gmail.com>
2014-02-01 20:20:09 +0100

    Changed object assignment to be nested.

--- Visual Novel Editor/XMLTester/MainPage.xaml.cs
+++ Visual Novel Editor/XMLTester/MainPage.xaml.cs
@@ -29,70 +29,81 @@ namespace XMLTester
             this.InitializeComponent();
             LoadTask();
         }
+
         public async void LoadTask()
         {
-            //Game mygame = new Game();
-            //mygame.Name = "Super Stream Fighter";
-            //mygame.Author = "David Patry";
-            //mygame.SceneList.Add(new Scene());
-
-            //Game mygame2 = await FileUtil.LoadGame();
-            //Console.Text = mygame2.Name + "\n" + mygame2.Author+ "\n";
-
-            Game game = new Game();
-            game.Name = "Super Stream Fighter";
-            game.Author = "David Patry";
-            game.Scenes = new List<Scene>();
-
-            Scene scene = new Scene();
-            scene.Id = Guid.NewGuid().ToString();
-            scene.BackgroundImage = "A.jpg";
-            scene.BackgroundMusic = "B.wav";
-            scene.Instances = new List<Instance>();
-
-            Instance inst = new Instance();
-            Dialog d = new Dialog();
-            d.Text = "Sample Text";
-            inst.Dialog = d;
-            Character c = new Character();
-            c.Name = "Aaron";
-            inst.Characters = new List<Character>();
-            inst.Characters.Add(c);
-
-            Instance inst2 = new Instance();
-            Dialog d2 = new Dialog();
-            d2.Text = "Test Text";
-            inst2.Dialog = d2;
-            Character c2 = new Character();
-            c2.Name = "Brandon";
-            inst2.Characters = new List<Character>();
-            inst2.Characters.Add(c2);
-
-            scene.Instances.Add(inst);
-            scene.Instances.Add(inst2);
-
-            Scene scene2 = new Scene();
-            scene2.Id = Guid.NewGuid().ToString();
-            scene.BackgroundImage = "C.png";
-            scene.BackgroundMusic = "D.wav";
-            scene2.Instances = new List<Instance>();
-
-            Instance inst3 = new Instance();
-            Dialog d3 = new Dialog();
-            d3.Text = "Test";
-            inst3.Dialog = d3;
-            Character c3 = new Character();
-            c3.Name = "Omar";
-            Character c4 = new Character();
-            c4.Name = "David";
-            inst3.Characters = new List<Character>();
-            inst3.Characters.Add(c3);
-            inst3.Characters.Add(c4);
-
-            scene2.Instances.Add(inst3);
-
-            game.Scenes.Add(scene);
-            game.Scenes.Add(scene2);
+            Game game = new Game()
+            {
+                Name = "Super Stream Fighter",
+                Author = "David Patry",
+                Scenes = new List<Scene>()
+                {
+                    new Scene()
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        BackgroundImage = "A.jpg",
+                        BackgroundMusic = "B.wav",
+                        Instances = new List<Instance>()
+                        {
+                            new Instance()
+                            {
+                                Dialog = new Dialog()
+                                {
+                                    Text = "Sample Text"
+                                },
+                                Characters = new List<Character>()
+                                { 
+                                    new Character()
+                                    {
+                                        Name = "Aaron"
+                                    }
+                                }
+                            },
+                            new Instance()
+                            {
+                                Dialog = new Dialog()
+                                {
+                                    Text = "Test Text"
+                                },
+                                Characters = new List<Character>()
+                                {
+                                    new Character()
+                                    {
+                                        Name = "Brandon"
+                                    }
+                                }
+                            }
+                        }
+                    },
+                    new Scene()
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        BackgroundImage = "C.png",
+                        BackgroundMusic = "D.wav",
+                        Instances = new List<Instance>()
+                        {
+                            new Instance()
+                            {
+                                Dialog = new Dialog()
+                                {
+                                    Text = "Test"
+                                },
+                                Characters = new List<Character>()
+                                {
+                                    new Character()
+                                    {
+                                        Name = "Omar"
+                                    },
+                                    new Character()
+                                    {
+                                        Name = "David"
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            };
 
             await FileUtil.SaveGame(game);
             Game newGame = await FileUtil.LoadGame();
